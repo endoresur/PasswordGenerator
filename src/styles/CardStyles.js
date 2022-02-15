fee5718491d1adda287fae135dd00a87796860bd
endoresur <mlhv2lv@gmail.com>
2022-02-15 23:32:59 +0800

    Styled background and main card

---
+++ src/styles/CardStyles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const GlassCard = styled.div`
+    height: 90%;
+    width: 80%;
+
+    background: rgba(255, 255, 255, 0.192);
+    backdrop-filter: blur(10px) saturate(80%);
+    border-radius: 15px;
+    border: 1px solid rgba(43, 43, 43, 0.568);
+`
+
+export {GlassCard};
