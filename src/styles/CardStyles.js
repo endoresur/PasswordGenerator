3176d6e167701f887e31225172b060749b944aaf
endoresur <mlhv2lv@gmail.com>
2022-02-15 23:49:53 +0800

    Add glass glow

--- src/styles/CardStyles.js
+++ src/styles/CardStyles.js
@@ -7,7 +7,7 @@ const GlassCard = styled.div`
     background: rgba(255, 255, 255, 0.192);
     backdrop-filter: blur(10px) saturate(80%);
     border-radius: 15px;
-    border: 1px solid rgba(43, 43, 43, 0.568);
+    box-shadow: 5px 5px 20px 5px rgba(196, 196, 196, .2);
 `
 
 export {GlassCard};
