aaae04044c1c14ab0f2ec63e5e2286ef21d547ac
endoresur <mlhv2lv@gmail.com>
2022-02-22 16:29:15 +0800

    Separated settings and letters into separate files

---
+++ src/data/letters.js
@@ -0,0 +1,7 @@
+
+const uppercase = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+const lowercase = 'qwertyuiopasdfghjklzxcvbnm';
+const numbers = '1234567890';
+const symbols = ',./<>?;"|{}[]~`!@#$%^&*()_-+=';
+
+export {uppercase, lowercase, numbers, symbols};
---
+++ src/data/options.js
@@ -0,0 +1,15 @@
+
+const choices = [
+    { id: 1, item: 'Easy to say', checked: false },
+    { id: 2, item: 'Easy to read', checked: false },
+    { id: 3, item: 'All characters', checked: true }
+];
+
+const variation = [
+    { id: 1, item: 'Uppercase', checked: false },
+    { id: 2, item: 'Lowercase', checked: true },
+    { id: 3, item: 'Numbers', checked: false },
+    { id: 4, item: 'Symbols', checked: true }
+];
+
+export {choices as Choices, variation as Variation};
